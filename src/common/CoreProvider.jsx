e69c684c624e58f8b3950d8dfcf6040bfdd1fc17
ducnp <phuduccva@gmail.com>
2023-04-24 22:53:59 +0700

    log

--- src/common/CoreProvider.jsx
+++ src/common/CoreProvider.jsx
@@ -3,6 +3,7 @@ import React from "react";
 export const CoreContext = React.createContext();
 
 export const CoreContextProvider = (props) => {
+  console.log('============= props',props)
   return (
     <CoreContext.Provider value={props}>{props.children}</CoreContext.Provider>
   );
